157d435ca66a70de95f1803a844cf4f14a45f386
Zhenya Peteliev <petelev.zhenya@gmail.com>
2019-01-18 02:15:21 +0200

    Implement basic project structure.

---
+++ AngularGradientView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AngularGradientView
+//
+//  Created by Zhenya Peteliev on 1/18/19.
+//  Copyright © 2019 Zhenya Peteliev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AngularGradientView/Controllers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AngularGradientView
+//
+//  Created by Zhenya Peteliev on 1/18/19.
+//  Copyright © 2019 Zhenya Peteliev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
